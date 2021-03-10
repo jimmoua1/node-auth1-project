dbbd51cd865a13e0949a5b801913dda502a4eab8
Asterisk@DESKTOP-5V275IP <tsheejmaus@yahoo.com>
2021-03-10 12:41:53 -0800

    reforked -npm issues

--- api/server.js
+++ api/server.js
@@ -2,6 +2,10 @@ const express = require("express");
 const helmet = require("helmet");
 const cors = require("cors");
 
+const userRouter = require('./users/users-router')
+const authRouter = require('./auth/auth-router')
+const session = require('express-session')
+const KnexSessionStore = require('connect-session-knex')(session)
 /**
   Do what needs to be done to support sessions with the `express-session` package!
   To respect users' privacy, do NOT send them a cookie unless they log in.
@@ -16,11 +20,32 @@ const cors = require("cors");
  */
 
 const server = express();
-
-server.use(helmet());
 server.use(express.json());
-server.use(cors());
+//////////////////////////////////////////////////////
+server.use(session({
+  name: "monkey", 
+  secret: "keep it secret, keep it safe!",
+  cookie: {
+  maxAge: 1000 * 60 * 60, 
+  secure: false, 
+  httpOnly: true, 
+},
+  resave: false, 
+  saveUninitialized: false, 
 
+  store: new KnexSessionStore({
+  knex: require('../database/dbConfig'),
+  tablename: 'sessions', 
+  sidfieldname: 'sid',
+  createtable: true, 
+  clearInterval: 1000 * 60 * 60,
+}),
+}))
+//////////////////////////////////////////////////////
+server.use(helmet());
+server.use(cors());
+server.use('/api/users', userRouter)
+server.use('/api/auth', authRouter)
 server.get("/", (req, res) => {
   res.json({ api: "up" });
 });
